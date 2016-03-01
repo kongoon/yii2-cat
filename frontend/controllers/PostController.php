2daebff61e6be44f4c9df7a161859060e1cbf18a
Manop Kongoon <kongoon@hotmail.com>
2016-03-01 11:37:20 +0700

    create PostController

---
+++ frontend/controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+namespace fontend\controllers;
+use yii\web\Controller;
+use frontend\models\Post;
+use yii\data\ActiveDataProvider;
+
+class PostController extends Controller
+{
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Post::find(),
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC, //SORT_ASC
+                ]
+            ]
+        ]);
+        return $this->render('index', [
+            'dataProvider' => $dataProvider
+        ]);
+    }
+}
