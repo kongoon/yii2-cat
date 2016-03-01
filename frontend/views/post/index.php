dc7632b8fc601766b07f614cf981a20914e969a5
Manop Kongoon <kongoon@hotmail.com>
2016-03-01 11:52:42 +0700

    update index

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -3,5 +3,17 @@ use yii\grid\GridView;
 ?>
 <h1>Post List</h1>
 <?=GridView::widget([
-    'dataProvider' => $dataProvider
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        'title',
+        'created_at:datetime',
+        'updated_at:datetime',
+        //'user.username',
+        [
+            'attribute' => 'user_id',
+            'value' => 'user.username',
+        ],
+        ['class' => 'yii\grid\ActionColumn']
+    ]
 ])?>
